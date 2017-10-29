727ccbcb322c95b53ca1a6566fe4848c3d732996
Mikhail Klementyev <jollheef@riseup.net>
2017-10-29 13:49:47 +0300

    Automatic Upgrades

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,8 @@
     shell = "/run/current-system/sw/bin/zsh";
   };
 
+  system.autoUpgrade.enable = true;
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
