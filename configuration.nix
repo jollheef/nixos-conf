da9e10ec68b2173cfffe9d81cd8221b05590f292
Mikhail Klementyev <jollheef@riseup.net>
2017-10-30 21:05:34 +0300

    Add feh

--- configuration.nix
+++ configuration.nix
@@ -42,7 +42,7 @@
   environment.systemPackages = with pkgs; [
     # utils
     wget tmux zsh vim emacs htop acpi bc
-    gnupg pinentry git pass unzip w3m whois dnsutils
+    gnupg pinentry git pass unzip w3m whois dnsutils feh
     # dev
     go gnumake gcc clang clang-analyzer global
     # base x
