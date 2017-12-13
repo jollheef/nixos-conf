9f2167f76cd71279e1c18307103ff62620f5d84b
Mikhail Klementyev <jollheef@riseup.net>
2017-12-13 05:17:23 +0300

    Add sshfs

--- configuration.nix
+++ configuration.nix
@@ -43,7 +43,7 @@
     # utils
     wget tmux zsh vim emacs htop acpi bc p7zip mpv
     gnupg pinentry git pass unzip w3m whois dnsutils feh
-    parted iotop nmap tldr
+    parted iotop nmap tldr sshfs
     # dev
     go gnumake gcc clang clang-analyzer global
     python2Full python3Full python27Packages.ipython python36Packages.ipython
